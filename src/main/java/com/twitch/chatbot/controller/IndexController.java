acbce08d74201841449fd4da5ad3e3ac7844c092
Anh Nguyen <namanhnguyenit@gmail.com>
2017-02-25 12:54:56 +0700

    Add index controller

--- src/main/java/com/twitch/chatbot/controller/IndexController.java
+++ src/main/java/com/twitch/chatbot/controller/IndexController.java
@@ -1,7 +1,18 @@
 package com.twitch.chatbot.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by leptepkt on 25/02/2017.
  */
+@RestController
 public class IndexController {
+    @RequestMapping("/")
+    public ResponseEntity<String> index() {
+        return new ResponseEntity<String>("Welcome to Twitch chatbot", HttpStatus.OK);
+    }
 }
